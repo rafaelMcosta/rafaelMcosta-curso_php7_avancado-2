a8d5137dafaad97c43a984b23fdfb28a3b57a90d
Rafael Alves <rafaelmachadocosta@hotmail.com>
2021-03-07 17:17:12 -0300

    Conhecendo os Operadores do PHP

---
+++ operadores/exemplo-04.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 55.0;
+$b = 55;
+
+var_dump($a > $b); //retorna um booleano
+
+echo "<br>";
+
+var_dump($a < $b);
+
+echo "<br>";
+
+var_dump($a == $b); //verifica se os valores são iguais idependente do tipo
+
+echo "<br>";
+
+var_dump($a === $b); //verifica o tipo e o valor se são iguais
+
+echo "<br>";
+
+var_dump($a != $b);
+
+echo "<br>";
+
+var_dump($a !== $b);
+
+?>
---
+++ operadores/exemplo-05.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = 35;
+$b = 60;
+
+var_dump($a <=> $b); //operador space ship; se a for maior resultado é 1, se a e b iguais o resultado é 0, se b maior que a resultado é -1
+
+?>
