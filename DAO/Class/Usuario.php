fcf1afdb23b61bfb6082ec58efd4117f779b2ab5
Rafael Alves <rafaelmachadocosta@hotmail.com>
2021-03-29 20:38:31 -0300

    PDO - DAO - SELECT - LIST - INSERT

--- DAO/Class/Usuario.php
+++ DAO/Class/Usuario.php
@@ -49,12 +49,7 @@ class Usuario {
 
 		if(count($results) > 0){
 
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
 
 		}
 
@@ -89,18 +84,48 @@ class Usuario {
 
 		if(count($results) > 0){
 
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
 
 		}else{
 
 			throw new Exception("Login e/ou senha inválidos");
 
 		}
+
+	}
+
+	public function setData($data){
+
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+	}
+
+	public function insert(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", 
+			array(
+				':LOGIN'=>$this->getDeslogin(),
+				':PASSWORD'=>$this->getDessenha()
+			));
+
+		if(count($results) > 0){
+
+			$this->setData($results[0]);
+
+		}
+
+	}
+
+	//Aspas setando os valores vazios caso não seja passado valores na hora da criação do objeto
+	public function __construct($login = "", $password = ""){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
 
 	}
 
