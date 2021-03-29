48e62b018e87995f477bd70aeb7941f314bc057b
Rafael Alves <rafaelmachadocosta@hotmail.com>
2021-03-29 20:10:05 -0300

    Executando a classe Usuario.php

--- DAO/index.php
+++ DAO/index.php
@@ -2,17 +2,35 @@
 
 require_once("config.php");
 
+//Carrrega todos os usuários do banco passando a query para o método select
 /*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
-*/
+echo json_encode($usuarios);*/
 
-$root = new Usuario();
+//Carrega apenas o usuário com id 3
+/*$root = new Usuario();
 
 $root->loadById(3);
 
-echo $root;
+echo $root;*/
+
+//Carrega todos os usuários utilizando um método estático getList()
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+//Carrega uma lista de usuários buscando pelo login
+/*$search = Usuario::search("jo");
+
+echo json_encode($search);*/
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
+
 
 ?>
