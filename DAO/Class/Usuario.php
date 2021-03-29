6998e61d90dcdf3489bbfd5dd958b574db19aff2
Rafael Alves <rafaelmachadocosta@hotmail.com>
2021-03-29 20:59:00 -0300

    PDO - DAO - SELECT - LIST - INSERT - UPDATE - DELETE

--- DAO/Class/Usuario.php
+++ DAO/Class/Usuario.php
@@ -136,6 +136,21 @@ class Usuario {
 
 	}
 
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+			':ID'=>$this->getIdusuario()
+		));
+
+		$this->setIdusuario(0);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
+
+	}
+
 	//Aspas setando os valores vazios caso não seja passado valores na hora da criação do objeto
 	public function __construct($login = "", $password = ""){
 
